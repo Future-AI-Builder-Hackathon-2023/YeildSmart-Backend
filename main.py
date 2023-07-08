b6226139ecff3f39cf1422ff7e20f5a847751da5
Akash-Tandale001 <ak.tandale123@gmail.com>
2023-07-08 15:57:47 +0530

    created predictCrop POST API

--- main.py
+++ main.py
@@ -9,19 +9,39 @@ origins = ["*"]
 
 app.add_midleware(
     CORSMiddleware,
-    allow_origins = origins,
-    allow_credentials = True,
+    allow_origins=origins,
+    allow_credentials=True,
 )
 
 cropRecomendation = pd.read_csv('./crop_prediction.csv')
 
-better_model = pickle.load(open('./crop_prediction.pkl','rb'))
+better_model = pickle.load(open('./crop_prediction.pkl', 'rb'))
+
 
 class cropInfo(BaseModel):
     nitrogen: int
     phosphorus: int
-    potassium:int
+    potassium: int
     temperature: int
     humidity: int
     ph: int
     rainfall: int
+
+
+@app.post('/predictCrop')
+async def predictCrop(cropInfo: cropInfo):
+    nitrogen_value = cropInfo.nitrogen
+    phosphorus_value = cropInfo.phosphorus
+    potassium_value = cropInfo.potassium
+    temperature_value = cropInfo.temperature
+    humidity_value = cropInfo.humidity
+    ph_value = cropInfo.ph
+    rainfall_value = cropInfo.rainfall
+
+    prediction = better_model.predict(
+        pd.DataFrame([[nitrogen_value, phosphorus_value, potassium_value, temperature_value, humidity_value, ph_value, rainfall_value]],
+                     columns=['N', 'P', 'K', 'temperature', 'humidity', 'ph', 'rainfall']
+                     ))
+    print(prediction)
+
+    return {"result": prediction[0]}
